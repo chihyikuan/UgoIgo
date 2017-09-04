f8817424e72e5878e7cd885a5e707541d71a7147
chihyikuan <kz1991@hotmail.com>
2017-09-05 01:14:05 +0800

    add Home page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Todo from './todo'
+import Home from './Home'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome Alisa</h2>
-          <div>This is our Todo List</div>
+          <div className="App-logo" />
+          <h2>Happy Birthday Alisa</h2>
         </div>
       
-        <Todo/>
+        <Home/>
       </div>
     );
   }
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import "./Home.css"
+class Home extends Component {
+    render() {
+        return (
+            <div id="Home">
+                123
+            </div>
+        );
+    }
+}
+
+Home.propTypes = {
+
+};
+
+export default Home;
