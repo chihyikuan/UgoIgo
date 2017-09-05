9f27dcc458fb83fd61451f58b7fa6d21dd96f735
chihyikuan <kz1991@hotmail.com>
2017-09-05 15:28:34 +0800

    Add  logo effect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Todo from './todo'
 import Home from './Home'
 class App extends Component {
+  state = {
+    logoClick: false
+  }
+
+  handleLogoClick = () => {
+    this.setState({
+      logoClick: true
+    })
+    this.playSound()
+
+    setTimeout(() => {
+      this.setState({
+        logoClick: false
+      })
+    }, 3000);
+  }
+
+  playSound = () => {
+    let aud = this.refs.audio
+    // let audioCount = 0;
+    // let maxAudioCount = 1;
+    // aud.onended = () => {
+    //   if (audioCount < maxAudioCount - 1) {
+    //     aud.play()
+    //     audioCount += 1;
+    //   } 
+    // }
+    aud.play()
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <div className="App-logo" />
+          <div className={this.state.logoClick ? "App-logo-click" : "App-logo"} onClick={this.handleLogoClick} />
+          <audio ref="audio" >
+            <source src="logoSound.wav" type="audio/wav" />
+          </audio>
           <h2>Happy Birthday Alisa</h2>
         </div>
-      
-        <Home/>
+
+        <Home />
       </div>
     );
   }
